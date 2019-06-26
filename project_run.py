8e75caa36a7acb1ede421849b2b1c048590aee0f
caiyueliang <393900414@qq.com>
2019-06-26 15:10:44 +0800

    update

--- project_run.py
+++ project_run.py
@@ -1,4 +1,32 @@
 import mlflow
 
-mlflow.projects.run(uri="./", entry_point="main", parameters={"alpha": 0.3})
-mlflow.projects.run(uri="./", entry_point="train_1", parameters={"alpha": 0.3})
+
+def run_main():
+    parameters = {"alpha": 0.3}
+    submitted_run = mlflow.projects.run(uri="./", entry_point="main", parameters=parameters)
+    run_id = submitted_run.run_id
+    mlflow_service = mlflow.tracking.MlflowClient()
+    # run_infos = mlflow_service.list_run_infos(
+    #     experiment_id=file_store.FileStore.DEFAULT_EXPERIMENT_ID,
+    #     run_view_type=ViewType.ACTIVE_ONLY)
+    run = mlflow_service.get_run(run_id)
+    print("[run_main] run_id ", run_id, run.data.params)
+    # assert run.data.params == parameters
+
+
+def run_train_1():
+    parameters = {"alpha": 0.4}
+    submitted_run = mlflow.projects.run(uri="./", entry_point="train_1", parameters=parameters)
+    run_id = submitted_run.run_id
+    mlflow_service = mlflow.tracking.MlflowClient()
+    # run_infos = mlflow_service.list_run_infos(
+    #     experiment_id=file_store.FileStore.DEFAULT_EXPERIMENT_ID,
+    #     run_view_type=ViewType.ACTIVE_ONLY)
+    run = mlflow_service.get_run(run_id)
+    print("[run_train_1] run_id ", run_id, run.data.params)
+    # assert run.data.params == parameters
+
+
+if __name__ == '__main__':
+    run_main()
+    run_train_1()
