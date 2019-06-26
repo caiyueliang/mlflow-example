ef6d5a6b36c5d4231fc6d1acaf9543f6e0698c21
caiyueliang <393900414@qq.com>
2019-06-26 11:47:17 +0800

    update experiment.py

--- experiment.py
+++ experiment.py
@@ -3,6 +3,7 @@ from mlflow.tracking import MlflowClient
 
 client = MlflowClient()
 experiments = client.list_experiments()                 # returns a list of mlflow.entities.Experiment
+print("[experiments] %s" % experiments)
 run = client.create_run(experiments[0].experiment_id)   # returns mlflow.entities.Run
 client.log_param(run.info.run_id, "hello", "world")
 client.set_terminated(run.info.run_id)
